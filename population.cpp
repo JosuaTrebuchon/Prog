04901ddc687a541f441948a731d6485660d43800
josua.trebuchon@etu.umontpellier.fr <josua.trebuchon@etu.umontpellier.fr@w7.fdsetu.infra.umontpellier.fr>
2019-10-16 12:53:28 +0200

    Fin populationVivante

--- population.cpp
+++ population.cpp
@@ -7,7 +7,7 @@
 		std::terminate();\
 	}
 
-Population::printCell(size_t i, size_t j){
+void Population::printCell(size_t i, size_t j){
 	CHECK_BOUND(i, j);
 	T[i][j].print();
 }
@@ -17,10 +17,10 @@ Population Population::next() const{
 	for(size_t i=0; i<N; i++){
 		for(size_t j=0; j<N; j++){
 			size_t voisin = nb_voisins_vivants(i, j);
-			(pop[i][j]).setVivante(voisin==3 || (voisin==2 && T[i][j].getVivante()));
+			(pop.T[i][j]).setEtat(voisin==3 || (voisin==2 && T[i][j].getEtat()));
 		}
 	}
-	
+
 	return pop;
 }
 
@@ -29,7 +29,7 @@ Population::Population(){
 		for(size_t j =0; j<N; j++){
 			T[i][j].setX(i);
 			T[i][j].setX(j);
-			T[i][j].setVivante(false);
+			T[i][j].setEtat(false);
 		}
 	}
 }
@@ -38,7 +38,7 @@ int Population::nb_cellules(Cellule::Couleur c) const{
 	int cpt = 0;
 	for(size_t i=0; i<N; i++){
 		for(size_t j=0; j<N; j++){
-			cpt += (estdeCouleur(c, &T[i][j]) ? 1 : 0);
+			cpt += (estDeCouleur(c, T[i][j]) ? 1 : 0);
 		}
 	}
 	return cpt;
@@ -48,13 +48,45 @@ size_t Population::nb_deces()const{
 	return nb_cellules(Cellule::rouge)+nb_cellules(Cellule::jaune);
 }
 
+size_t Population::nb_mortes() const{
+	return nb_cellules(Cellule::noir);
+}
 size_t Population::nb_vivantes() const{
 	return N*N - nb_mortes();
 }
 
-size_t Population::nb_mortes() const{
-	return nb_cellules(Cellule::noir);
+/*
+On peut utiliser la méthode estVoisine mais est plus longue à l'éxecution.
+*/
+size_t Population::nb_voisins_vivants(size_t i, size_t j) const{
+	size_t cmpt = 0;
+	size_t imin, imax, jmin, jmax;
+	imin = i==0 ? i : i-1;
+	imax = i==0 ? i : i+1;
+	jmin = j==0 ? j : j-1;
+	jmax = j==0 ? j : j+1;
+	for(size_t k=imin; k<imax; k++){
+		for(size_t l=jmin; l<jmax; l++){
+			if(T[k][l].getEtat()){
+				cmpt++;
+			}
+		}
+	}
+	return cmpt - (T[i][j].getEtat() ? 1 : 0);
 }
+
+void Population::updateColors(){
+	for(size_t k=0; k<N; k++){
+		for(size_t l=0; l<N; l++){
+			size_t voisin = nb_voisins_vivants(k,l);
+
+			if(voisin !=3 && voisin != 2 && T[k][l].getEtat()){
+				T[k][l].mortPrematuree();
+			}
+		}
+	}
+}
+
 /*
 Compte le nb de cellules qui viennent de naitre et qui ne vont pas mourire.
 
--- population.h
+++ population.h
@@ -15,13 +15,15 @@ class Population{
 		void init(size_t i);//srand(time(NULL)); i=rand()%taille; y=rand()%taille;
 
 		size_t nb_vivantes()const;
+		size_t nb_mortes() const;
 		size_t nb_deces() const; // vont mourir
-		size_t nb_morts() const;
 		size_t nb_naissances() const;
+		size_t nb_voisins_vivants(size_t i, size_t j) const;
 		/*Cellule getCelluleCopie(size_t i, size_t j) const;
 		const Cellule& getCellule(size_t i, size_t j) const;
-		void updateColors();// après initialisation, changement des couleurs des cellules vivantes.
 		*/
+		void updateColors();// après initialisation, changement des couleurs des cellules vivantes.
+		
 
 		Cellule getCelluleCopie(size_t i, size_t j) const;
 		const Cellule& getCellule(size_t i, size_t j) const;
