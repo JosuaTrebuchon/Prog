91f5d40541e553134ef81d6cc2052fde074999fa
josua.trebuchon@etu.umontpellier.fr <josua.trebuchon@etu.umontpellier.fr@w7.fdsetu.infra.umontpellier.fr>
2019-10-23 11:38:39 +0200

    fin exo1

--- TD4/tee.cpp
+++ TD4/tee.cpp
@@ -4,27 +4,54 @@
 
 using namespace std;
 
+// Pour ./a.out arg1 arg2 arg3
+// Copie dans arg1 l'entrée standard (ce que l'user écrit)
+// Puis on copie le conetenue de arg1 dans tous les autres arg
+
 int main(int argc, char **argv){
 	if(argc<2){
-		cerr<<"error"<<endl;
+		cerr<<"error 1"<<endl;
 		return 1;
 	}
-	for(int i=0; i< argc; i++){
-		// Si le fichier n'existe pas le créer
-		ofstream fich(argv[i]);
-		// Vérification de l'ouverture
-		if(!fich.is_open()){
-			return 2;
+	string s = "";
+	string exit = "FIN\0";
+	// Si le fichier n'existe pas le créer
+	ofstream fich(argv[1]);
+	// Vérification de l'ouverture
+	if(!fich.is_open()){
+		cerr<<"error 2"<<endl;
+		return 2;
+	}
+	while(!cin.eof()){
+		getline(cin, s);
+		if(s.compare(exit)==0){break;}
+		if(!cin.eof() || !s.empty()){
+			fich<<s<<endl;
+			cout<<s<<endl;
 		}
+	}
+	fich.close();
 
-		while(!cin.eof()){
+	for(int i=2; i< argc; i++){
+		ifstream fich1(argv[1]);
+		ofstream fich2(argv[i]);
+		if(!fich1.is_open()){
+			cerr<<"pas ouvrable"<<endl;
+			return 401;
+		}if(!fich2.is_open()){
+			cerr<<"pas ouvrable"<<endl;
+			return 402;
+		}
+		while(!fich1.eof()){
 			string s;
-			getLine(fich, s);
+			getline(fich1,s);
+			//if(s.compare(exit)==0){break;}
 			if(!s.empty()){
-				fich<<s<<endl;
-				cout<<s<<endl;
+				fich2<<s<<endl;
 			}
 		}
-		fich.close();
+		fich1.close();
+		fich2.close();
 	}
+	return 0;
 }
